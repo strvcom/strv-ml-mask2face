f9975e872b8e031100842e203a6a10b1dd3c38a9
Lukas Koucky <lukas.koucky@strv.com>
2020-12-23 10:51:16 +0100

    Updated model and architecture after pair programming with Jan Maly. Architecture is now parameterized by convolution filters

--- utils/architectures.py
+++ utils/architectures.py
@@ -2,46 +2,51 @@ from tensorflow.keras.layers import *
 from tensorflow.keras.models import Model
 import tensorflow as tf
 
+
 class UNet_segmentation:
     """ UNet architecture from following github notebook for image segmentation:
         https://github.com/nikhilroxtomar/UNet-Segmentation-in-Keras-TensorFlow/blob/master/unet-segmentation.ipynb
-         
+
         1 963 043 parameters (4,589,283 parameters with embeding from MobileNet in bottleneck layer)
     """
-    
-    def __init__(self, use_embeding=True, input_size=(256, 256, 3)):
+
+    def __init__(self, use_embeding=True, input_size=(256, 256, 3), filters=(16, 32, 64, 128, 256)):
+
+        # TODO - different losses
+
         p0 = Input(input_size)
-        f = [16, 32, 64, 128, 256]
-        
+
         if use_embeding:
             mobilenet_model = tf.keras.applications.MobileNetV2(input_shape=(256, 256, 3),
-                                                   include_top=False,
-                                                   weights='imagenet')
+                                                                include_top=False,
+                                                                weights='imagenet')
             mobilenet_model.trainable = False
             for layer in mobilenet_model.layers:
-                layer.trainable=False
+                layer.trainable = False
 
             mn1 = mobilenet_model(p0)
-            mn1 = Reshape((16,16,320))(mn1)      
-        
-        c1, p1 = UNet_segmentation.down_block(p0, f[0]) #128 -> 64
-        c2, p2 = UNet_segmentation.down_block(p1, f[1]) #64 -> 32
-        c3, p3 = UNet_segmentation.down_block(p2, f[2]) #32 -> 16
-        c4, p4 = UNet_segmentation.down_block(p3, f[3]) #16->8
+            mn1 = Reshape((16, 16, 320))(mn1)
 
-        bn = UNet_segmentation.bottleneck(p4, f[4])
-        
+        conv_outputs = []
+        int_layer = p0
+
+        for f in filters:
+            conv_output, int_layer = UNet_segmentation.down_block(int_layer, f)
+            conv_outputs.append(conv_output)
+
+        int_layer = UNet_segmentation.bottleneck(int_layer, filters[-1])
+
         if use_embeding:
-            bn = Concatenate()([bn, mn1])
-        
-        u1 = UNet_segmentation.up_block(bn, c4, f[3]) #8 -> 16
-        u2 = UNet_segmentation.up_block(u1, c3, f[2]) #16 -> 32
-        u3 = UNet_segmentation.up_block(u2, c2, f[1]) #32 -> 64
-        u4 = UNet_segmentation.up_block(u3, c1, f[0]) #64 -> 128
+            int_layer = Concatenate()([int_layer, mn1])
 
-        outputs = Conv2D(3, (1, 1), padding="same", activation="sigmoid")(u4)
+        conv_outputs = list(reversed(conv_outputs))
+        for i, f in enumerate(reversed(filters)):
+            int_layer = UNet_segmentation.up_block(int_layer, conv_outputs[i], f)
+
+        int_layer = Conv2D(filters[0] // 2, 3, padding="same", activation="relu")(int_layer)
+        outputs = Conv2D(3, (1, 1), padding="same", activation="sigmoid")(int_layer)
         self.model = Model(p0, outputs)
-    
+
     @staticmethod
     def down_block(x, filters, kernel_size=(3, 3), padding="same", strides=1):
         c = Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
@@ -52,9 +57,9 @@ class UNet_segmentation:
     @staticmethod
     def up_block(x, skip, filters, kernel_size=(3, 3), padding="same", strides=1):
         us = UpSampling2D((2, 2))(x)
-        concat = Concatenate()([us, skip])
+        c = Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(us)
+        concat = Concatenate()([c, skip])
         c = Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(concat)
-        c = Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
         return c
 
     @staticmethod
@@ -66,20 +71,21 @@ class UNet_segmentation:
     def get_model(self):
         return self.model
 
+
 class UNet_polyp():
     """ Model from polyp segmentation github
         https://github.com/nikhilroxtomar/Polyp-Segmentation-using-UNET-in-TensorFlow-2.0
-        
+
         414 435 parameters
     """
-    
+
     def __init__(self, input_size=(256, 256, 3)):
         num_filters = [16, 32, 48, 64]
         inputs = Input(input_size)
 
         skip_x = []
         x = inputs
-        
+
         ## Encoder
         for f in num_filters:
             x = UNet_polyp.conv_block(x, f)
@@ -103,7 +109,7 @@ class UNet_polyp():
         x = Activation("sigmoid")(x)
 
         self.model = Model(inputs, x)
-    
+
     @staticmethod
     def conv_block(x, num_filters):
         x = Conv2D(num_filters, (3, 3), padding="same")(x)
@@ -114,6 +120,6 @@ class UNet_polyp():
         x = BatchNormalization()(x)
         x = Activation("relu")(x)
         return x
-    
+
     def get_model(self):
         return self.model
--- utils/model.py
+++ utils/model.py
@@ -1,47 +1,52 @@
 import os
-import numpy as np
-import cv2
 from datetime import datetime
 from glob import glob
+
+import cv2
+import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger, TensorBoard
 from tensorflow.keras.utils import CustomObjectScope
+
 from utils.architectures import UNet_polyp, UNet_segmentation
+
 
 class Mask2FaceModel():
     """ Model for Mask2Face - removes mask from people faces using U-net neural network
     """
-    
-    def __init__(self, arch='polyp', use_embeding=True, input_size=(256, 256)):
+
+    def __init__(self, arch='polyp', use_embeding=True, input_size=(256, 256), filters=(16, 32, 64, 128, 256)):
         physical_devices = tf.config.experimental.list_physical_devices('GPU')
         tf.config.experimental.set_memory_growth(physical_devices[0], True)
         self.architecture = arch
         self.input_size = input_size
         self.model = None
         self.use_embeding = use_embeding
+        self.filters = filters
         self.build_model()
-    
+
     def load_model(self, model_path):
         with CustomObjectScope({'iou': Mask2FaceModel.iou}):
             self.model = tf.keras.models.load_model(model_path)
-        
+
     def build_model(self):
         if self.architecture == 'polyp':
             arch = UNet_polyp()
         else:
-            arch = UNet_segmentation(self.use_embeding)
+            arch = UNet_segmentation(self.use_embeding, filters=self.filters)
         self.model = arch.get_model()
 
-    def train(self, epochs=20, batch_size=20, loss_function='mse', learning_rate=1e-4, l1_weight=1):
+    def train(self, epochs=20, batch_size=20, loss_function='mse', learning_rate=1e-4, l1_weight=1,
+              predict_difference: bool = True):
         (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = Mask2FaceModel.load_train_data()
 
-        train_dataset = Mask2FaceModel.tf_dataset(train_x, train_y, batch=batch_size)
-        valid_dataset = Mask2FaceModel.tf_dataset(valid_x, valid_y, batch=batch_size)
+        train_dataset = Mask2FaceModel.tf_dataset(train_x, train_y, batch_size, predict_difference)
+        valid_dataset = Mask2FaceModel.tf_dataset(valid_x, valid_y, batch_size, predict_difference)
 
         opt = tf.keras.optimizers.Adam(learning_rate)
         metrics = ["acc", tf.keras.metrics.Recall(), tf.keras.metrics.Precision(), Mask2FaceModel.iou]
-        
+
         if loss_function == 'ssim_loss':
             @tf.function
             def ssim_loss(gt, y_pred, max_val=1.0):
@@ -57,21 +62,21 @@ class Mask2FaceModel():
 
             loss = ssim_l1_loss
         else:
-            loss = 'mse'
+            loss = loss_function
 
-    
         self.model.compile(loss=loss, optimizer=opt, metrics=metrics)
 
         callbacks = [
-            ModelCheckpoint(f'model_epochs-{epochs}_batch-{batch_size}_loss-{loss_function}_{Mask2FaceModel.get_datetime_string()}.h5'),
+            ModelCheckpoint(
+                f'model_epochs-{epochs}_batch-{batch_size}_loss-{loss_function}_{Mask2FaceModel.get_datetime_string()}.h5'),
             ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=4),
             CSVLogger("data.csv"),
             TensorBoard(),
-            EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=False)
+            EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
         ]
 
-        train_steps = len(train_x)//batch_size
-        valid_steps = len(valid_x)//batch_size
+        train_steps = len(train_x) // batch_size
+        valid_steps = len(valid_x) // batch_size
 
         if len(train_x) % batch_size != 0:
             train_steps += 1
@@ -79,42 +84,48 @@ class Mask2FaceModel():
             valid_steps += 1
 
         self.model.fit(train_dataset,
-            validation_data=valid_dataset,
-            epochs=epochs,
-            steps_per_epoch=train_steps,
-            validation_steps=valid_steps,
-            callbacks=callbacks)
-    
-    def predict_batch(self, batch_size=8):
-        (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = Mask2FaceModel.load_test_data()
-        test_dataset = Mask2FaceModel.tf_dataset(test_x, test_y, batch=batch_size)
+                       validation_data=valid_dataset,
+                       epochs=epochs,
+                       steps_per_epoch=train_steps,
+                       validation_steps=valid_steps,
+                       callbacks=callbacks)
 
-        test_steps = (len(test_x)//batch_size)
+    def predict_batch(self, batch_size=8, predict_difference: bool = True):
+        (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = Mask2FaceModel.load_test_data()
+
+        test_steps = (len(test_x) // batch_size)
         if len(test_x) % batch_size != 0:
             test_steps += 1
-        
+
         result_dir = f"data/results/{Mask2FaceModel.get_datetime_string()}/"
         os.mkdir(result_dir)
 
         for i, (x, y) in enumerate(zip(test_x, test_y)):
             x = Mask2FaceModel.read_image(x)
             y = Mask2FaceModel.read_image(y)
+
             y_pred = self.model.predict(np.expand_dims(x, axis=0))
+            if predict_difference:
+                y_pred = (y_pred * 2) - 1
+                y_pred = np.clip(x - y_pred.squeeze(axis=0), 0.0, 1.0)
+            else:
+                y_pred = y_pred.squeeze(axis=0)
             h, w, _ = x.shape
             white_line = np.ones((h, 10, 3)) * 255.0
 
             all_images = [
                 x * 255.0, white_line,
                 y * 255.0, white_line,
-                y_pred.squeeze(axis=0) * 255.0
+                y_pred * 255.0
             ]
             image = np.concatenate(all_images, axis=1)
             cv2.imwrite(os.path.join(result_dir, f"{i}.png"), image)
-            
+
     def summary(self):
         self.model.summary()
-        
+
     def predict(self, img_path):
+        # tODO - different loss function
         x = Mask2FaceModel.read_image(img_path)
         y_pred = self.model.predict(np.expand_dims(x, axis=0))
         h, w, _ = x.shape
@@ -125,11 +136,11 @@ class Mask2FaceModel():
             y_pred.squeeze(axis=0) * 255.0
         ]
         image = np.concatenate(all_images, axis=1)
-        cv2.imwrite(img_path.split('.')[0]+'_unmasked.png', image)
+        cv2.imwrite(img_path.split('.')[0] + '_unmasked.png', image)
 
         def summary(self):
             self.model.summary()
-        
+
     @staticmethod
     def get_datetime_string():
         now = datetime.now()
@@ -147,7 +158,7 @@ class Mask2FaceModel():
     def load_data(input_path, output_path, split=0.2):
         images = sorted(glob(os.path.join(input_path, "*.png")))
         masks = sorted(glob(os.path.join(output_path, "*.png")))
-        
+
         total_size = len(images)
         valid_size = int(split * total_size)
         test_size = int(split * total_size)
@@ -164,7 +175,7 @@ class Mask2FaceModel():
     def read_image(path):
         x = cv2.imread(path, cv2.IMREAD_COLOR)
         x = cv2.resize(x, (256, 256))
-        x = x/255.0
+        x = x / 255.0
         return x
 
     @staticmethod
@@ -172,7 +183,7 @@ class Mask2FaceModel():
         x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
         x = cv2.resize(x, (256, 256))
         if normalize:
-            x = x/255.0
+            x = x / 255.0
         x = np.expand_dims(x, axis=-1)
         return x
 
@@ -189,11 +200,17 @@ class Mask2FaceModel():
         return x, y
 
     @staticmethod
-    def tf_dataset(x, y, batch=8):
+    def tf_dataset(x, y, batch=8, predict_difference: bool = True):
         dataset = tf.data.Dataset.from_tensor_slices((x, y))
         dataset = dataset.map(Mask2FaceModel.tf_parse)
+
+        if predict_difference:
+            def map_output(img_in, img_target):
+                return img_in, (img_in - img_target + 1.0) / 2
+
+            dataset = dataset.map(map_output)
+
         dataset = dataset.batch(batch)
-        dataset = dataset.repeat()
         return dataset
 
     @staticmethod
@@ -211,8 +228,9 @@ class Mask2FaceModel():
             x = (intersection + 1e-15) / (union + 1e-15)
             x = x.astype(np.float32)
             return x
+
         return tf.numpy_function(f, [y_true, y_pred], tf.float32)
-    
+
 
 if __name__ == "__main__":
     model = Mask2FaceModel()
