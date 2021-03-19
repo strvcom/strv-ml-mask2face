16554afea96b4300882644ecfe85894de14fe8d4
Jan Maly <jan.maly@strv.com>
2021-03-19 11:40:29 +0100

    Added kernels as an additional parameter. Parameters of network changed.

--- utils/architectures.py
+++ utils/architectures.py
@@ -20,7 +20,7 @@ class BaseUNet(ABC):
 
     @staticmethod
     @abstractmethod
-    def build_model(input_size: Tuple[int, int, int], filters: Optional[Tuple] = None):
+    def build_model(input_size: Tuple[int, int, int], filters: Tuple, kernels: Tuple):
         pass
 
 
@@ -32,15 +32,29 @@ class UNet(Enum):
     DEFAULT_IMAGENET_EMBEDDING = 1
     RESNET = 3
 
-    def build_model(self, input_size: Tuple[int, int, int], filters: Optional[Tuple] = None) -> BaseUNet:
+    def build_model(self, input_size: Tuple[int, int, int], filters: Optional[Tuple] = None,
+                    kernels: Optional[Tuple] = None) -> BaseUNet:
+
+        # set default filters
+        if filters is None:
+            filters = (16, 32, 64, 128, 256)
+
+        # set default kernels
+        if kernels is None:
+            filters = (3 for _ in range(len(filters)))
+
+        # check kernels and filters
+        if len(filters) != len(kernels):
+            raise Exception('Kernels and filter count has to match.')
+
         if self == UNet.DEFAULT_IMAGENET_EMBEDDING:
             print('Using default UNet model with imagenet embedding')
-            return UNetDefault.build_model(input_size, filters, use_embedding=True)
+            return UNetDefault.build_model(input_size, filters, kernels, use_embedding=True)
         elif self == UNet.RESNET:
             print('Using UNet Resnet model')
-            return UNet_resnet.build_model(input_size, filters)
+            return UNet_resnet.build_model(input_size, filters, kernels)
         print('Using default UNet model')
-        return UNetDefault.build_model(input_size, filters, use_embedding=False)
+        return UNetDefault.build_model(input_size, filters, kernels, use_embedding=False)
 
 
 class UNet_resnet(BaseUNet):
@@ -49,48 +63,47 @@ class UNet_resnet(BaseUNet):
     """
 
     @staticmethod
-    def build_model(input_size: Tuple[int, int, int], filters: Optional[Tuple] = None):
-
-        # set default filters
-        if filters is None:
-            filters = (16, 32, 64, 128, 256)
+    def build_model(input_size: Tuple[int, int, int], filters: Tuple, kernels: Tuple):
 
         p0 = Input(shape=input_size)
         conv_outputs = []
-        first_layer = Conv2D(filters[0], 3, padding='same')(p0)
+        first_layer = Conv2D(filters[0], kernels[0], padding='same')(p0)
         int_layer = first_layer
         for i, f in enumerate(filters):
-            int_layer, skip = UNet_resnet.down_block(int_layer, f)
+            int_layer, skip = UNet_resnet.down_block(int_layer, f, kernels[i])
             conv_outputs.append(skip)
 
-        int_layer = UNet_resnet.bottleneck(int_layer, filters[-1])
+        int_layer = UNet_resnet.bottleneck(int_layer, filters[-1], kernels[-1])
 
         conv_outputs = list(reversed(conv_outputs))
         reversed_filter = list(reversed(filters))
+        reversed_kernels = list(reversed(kernels))
         for i, f in enumerate(reversed_filter):
             if i + 1 < len(reversed_filter):
                 num_filters_next = reversed_filter[i + 1]
+                num_kernels_next = reversed_kernels[i + 1]
             else:
                 num_filters_next = f
-            int_layer = UNet_resnet.up_block(int_layer, conv_outputs[i], f, num_filters_next)
+                num_kernels_next = reversed_kernels[i]
+            int_layer = UNet_resnet.up_block(int_layer, conv_outputs[i], f, num_filters_next, num_kernels_next)
 
         # concat. with the first layer
         int_layer = Concatenate()([first_layer, int_layer])
-        int_layer = Conv2D(filters[0], 3, padding="same", activation="relu")(int_layer)
+        int_layer = Conv2D(filters[0], kernels[0], padding="same", activation="relu")(int_layer)
         outputs = Conv2D(3, (1, 1), padding="same", activation="sigmoid")(int_layer)
         model = Model(p0, outputs)
         return UNet_resnet(model)
 
     @staticmethod
-    def down_block(x, num_filters: int = 64):
+    def down_block(x, num_filters: int = 64, kernel: int = 3):
         # down-sample inputs
-        x = Conv2D(num_filters, 3, padding='same', strides=2)(x)
+        x = Conv2D(num_filters, kernel, padding='same', strides=2)(x)
 
         # inner block
-        out = Conv2D(num_filters, 3, padding='same')(x)
+        out = Conv2D(num_filters, kernel, padding='same')(x)
         # out = BatchNormalization()(out)
         out = Activation('relu')(out)
-        out = Conv2D(num_filters, 3, padding='same')(out)
+        out = Conv2D(num_filters, kernel, padding='same')(out)
 
         # merge with the skip connection
         out = Add()([out, x])
@@ -98,16 +111,16 @@ class UNet_resnet(BaseUNet):
         return Activation('relu')(out), x
 
     @staticmethod
-    def up_block(x, skip, num_filters: int = 64, num_filters_next: int = 64):
+    def up_block(x, skip, num_filters: int = 64, num_filters_next: int = 64, kernel: int = 3):
 
         # add U-Net skip connection - before up-sampling
         concat = Concatenate()([x, skip])
 
         # inner block
-        out = Conv2D(num_filters, 3, padding='same')(concat)
+        out = Conv2D(num_filters, kernel, padding='same')(concat)
         # out = BatchNormalization()(out)
         out = Activation('relu')(out)
-        out = Conv2D(num_filters, 3, padding='same')(out)
+        out = Conv2D(num_filters, kernel, padding='same')(out)
 
         # merge with the skip connection
         out = Add()([out, x])
@@ -119,14 +132,14 @@ class UNet_resnet(BaseUNet):
 
         # up-sample
         # out = UpSampling2D((2, 2))(concat)
-        out = Conv2DTranspose(num_filters_next, 3, padding='same', strides=2)(concat)
-        out = Conv2D(num_filters_next, 3, padding='same')(out)
+        out = Conv2DTranspose(num_filters_next, kernel, padding='same', strides=2)(concat)
+        out = Conv2D(num_filters_next, kernel, padding='same')(out)
         # out = BatchNormalization()(out)
         return Activation('relu')(out)
 
     @staticmethod
-    def bottleneck(x, filters):
-        x = Conv2D(filters, 3, padding='same', name='bottleneck')(x)
+    def bottleneck(x, filters, kernel: int = 3):
+        x = Conv2D(filters, kernel, padding='same', name='bottleneck')(x)
         # x = BatchNormalization()(x)
         return Activation('relu')(x)
 
@@ -139,11 +152,7 @@ class UNetDefault(BaseUNet):
     """
 
     @staticmethod
-    def build_model(input_size: Tuple[int, int, int], filters: Optional[Tuple] = None, use_embedding: bool = True):
-
-        # set default filters
-        if filters is None:
-            filters = (16, 32, 64, 128, 256)
+    def build_model(input_size: Tuple[int, int, int], filters: Tuple, kernels: Tuple, use_embedding: bool = True):
 
         p0 = Input(input_size)
 
